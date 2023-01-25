4d217c8931fa71fea4302bd8ea4d60969ad7e378
Faisal ahmed <faisal2410@yahoo.com>
2023-01-25 22:08:03 +0600

    footer added

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -4,6 +4,7 @@ const Footer = () => {
     return (
         <div>
             <h1 className='bg-secondary text-white'>Hello From Footer</h1>
+            <h2>Design and Developed by Ostadb2</h2>
         </div>
     );
 };

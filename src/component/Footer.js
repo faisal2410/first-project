2813ebe56a56a22b2fc9ff7061784e8e11a712b2
Faisal ahmed <faisal2410@yahoo.com>
2023-01-25 22:06:17 +0600

    initial commit

---
+++ src/component/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <h1 className='bg-secondary text-white'>Hello From Footer</h1>
+        </div>
+    );
+};
+
+export default Footer;
